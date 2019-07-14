d21b2444a8fe639bbd678258d13cb21b33087a55
zeam2003 <zeam2003@outlook.com>
2019-07-14 16:57:43 -0300

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,25 @@
+import Server from './classes/server';
+import { SERVER_PORT } from './global/environment';
+import router  from './routes/router'; 
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+// Body Parser
+server.app.use( bodyParser.urlencoded( { extended: true }));
+server.app.use( bodyParser.json());
+
+// CORS
+server.app.use( cors({ origin:true, credentials: true}));
+
+// Rutas de Servicio
+server.app.use('/', router)
+
+
+
+
+server.start( () => {
+    console.log(`Servidor corriendo en el puerto ${ SERVER_PORT}`);
+});
